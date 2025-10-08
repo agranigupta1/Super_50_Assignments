a422503b31f392a0ded71d2aaadb7a2ddf12affd
Agrani Gupta <agranigupta683@gmail.com>
2025-10-08 15:06:57 +0530

    Updated MyString.java and Main.java files

--- MyString.java
+++ MyString.java
@@ -1,133 +1,145 @@
+
 /**
  * filename- string.java
  * description- This will perform most of the string operation 
  * author- Agrani Gupta
  */
 import java.util.*;
- public class MyString{
-    String s;
-    void setdata(String s){
-        this.s = s;
-    }
-    void showdata(){
-        System.out.println("the string is:" + s);
-    }
-    String append(String newString){
-        this.s = this.s + newString;
-        return s;
-    }
-  int countWords(){
-        
-       
-        int f=0;
-        int i=0;
-        String r="";
-        int cnt=0;
-        while(i<this.s.length()){
-          if(s.charAt(i)==' '){
-            cnt++;
-            i++;
-            r="";
-          }
-           while(s.charAt(i)==' '){
-             i++;
-           }
-            if(s.charAt(i)!=' '){
-              r+=s.charAt(i);
-           }
 
-           i++;
-        }
+public class MyString {
+  String s;
+
+  MyString(String s) {
+    this.s = s;
+  }
+
+  String append(String newString) {
+    this.s = this.s + newString;
+    return s;
+  }
+
+  int countWords() {
+    int index = 0;
+    String duplicate = "";
+    int cnt = 0;
+    while (index < this.s.length()) {
+      if (s.charAt(index) == ' ') {
         cnt++;
-        return cnt;
+        index++;
+        duplicate = "";
+      }
+      while (s.charAt(index) == ' ') {
+        index++;
+      }
+      if (s.charAt(index) != ' ') {
+        duplicate += s.charAt(index);
+      }
+
+      index++;
     }
-    boolean isPalindrome(){
-        int i = 0, j = s.length() - 1;
-        while(i <= j){
-            if(s.charAt(i) != s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
+    cnt++;
+    return cnt;
+  }
+
+  boolean isPalindrome() {
+    int i = 0, j = s.length() - 1;
+    while (i <= j) {
+      if (s.charAt(i) != s.charAt(j)) {
+        return false;
+      }
+      i++;
+      j--;
+    }
+    return true;
+  }
+
+  String replace(char a, char b) {
+    char[] arr = s.toCharArray();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == a) {
+        arr[i] = b;
+      }
+    }
+    s = new String(arr);
+    return s;
+  }
+
+  String splice(int start, int len) {
+    if (start < 0 || start >= s.length() || len < 0 || start + len > s.length()) {
+      System.out.println("invalid input");
+      return "";
+    }
+    String splicedString = s.substring(0, start) + s.substring(start + len);
+    return splicedString;
+  }
+
+  List<String> split() {
+    String a = "";
+    List<String> list = new ArrayList<>();
+    int i = 0;
+    while (i < s.length()) {
+      char x = s.charAt(i);
+      if (Character.isWhitespace(x)) {
+        if (a != null) {
+          list.add(a);
+          a = "";
         }
-        return true;
+      } else {
+        a += x;
+      }
+      i++;
     }
-    String replace(char a,char b){
-        char[] arr = s.toCharArray();
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == a){
-                arr[i] = b;
-            }
-        }
-        s = new String(arr);
-        return s;
+    list.add(a);
+    return list;
+  }
+
+  char maxrepeat() {
+    int[] arr = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+      char x = s.charAt(i);
+      arr[x - 'a']++;
     }
-    void splice(int start,int len){
-        if(start < 0 || start >= s.length() || len < 0 || start + len > s.length()){
-            System.out.println("invalid input");
-            return;
-        }
-        s = s.substring(0, start) + s.substring(start + len);
+    int maxx = 0;
+    char finalstring = 'a';
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > maxx) {
+        maxx = arr[i];
+        finalstring = (char) ('a' + i);
+      }
     }
-    List<String> split(){ 
-      String a=""; 
-      List<String> list = new ArrayList<>();
-       int i=0; 
-       while(i<s.length()){
-         char x=s.charAt(i);
-          if(Character.isWhitespace(x)){ 
-            if(a!=null){ list.add(a); a=""; }
-           } 
-           else{
-             a+=x; 
-          } 
-          i++; 
-        }
-         list.add(a);
-         return list;
-         }
-     
-       char maxrepeat(){
-         int[] arr = new int[26];
-          for(int i=0;i<s.length();i++){
-             char x=s.charAt(i); arr[x-'a']++; 
-            } 
-            int maxx=0; 
-            char fin='a';
-             for(int i=0;i<arr.length;i++){ 
-              if(arr[i]>maxx){
-                 maxx=arr[i];
-                 fin=(char)('a'+i); 
-                } 
-              } 
-              return fin;
-             }
-    String shift(int n){
-        //left shift
-         String r= s; 
-         splice(0,n); 
-         for(int i=0;i<n;i++){
-           s+=r.charAt(i); 
-          }
-           return s;
+    return finalstring;
+  }
+
+  String shift(int n) {
+    // left shift
+    String r = s;
+    splice(0, n);
+    for (int i = 0; i < n; i++) {
+      s += r.charAt(i);
     }
-    void sort(){
-      int arr[]=new int[26];
-       for(int i=0;i<s.length();i++){ 
-        char x=s.charAt(i); arr[x-'a']++; 
-      } 
-      String ans="";
-       for(int i=0;i<arr.length;i++){ 
-        for(int j=0;j<arr[i];j++){
-           ans+='a'+i; 
-          }
-           }
+    return s;
+  }
+
+  String sort() {
+    int arr[] = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+      char x = s.charAt(i);
+      arr[x - 'a']++;
     }
-    void reverse(){
-        String r="";
-        for(int i=s.length()-1;i>=0;i--){
-           r+=this.s.charAt(i);
-        }
-        this.s=r;
+    String answer = "";
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[i]; j++) {
+        answer += 'a' + i;
+      }
     }
+    return answer;
+  }
+
+  String reverse() {
+    String revString = "";
+    for (int i = s.length() - 1; i >= 0; i--) {
+      revString += this.s.charAt(i);
+    }
+    return revString;
+  }
 }
